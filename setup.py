6d21a58c11350168bd5eca88a3a438420e19a807
jstringara <jstringara@gmail.com>
2021-10-15 20:15:41 +0200

    Corretto setup e aggiunti file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 import py2exe
 
 setup(
-    data_files=[('',['Logo_aim-drive.ico'])],
+    data_files=[('',['Logo_aim-drive.ico','aim_watermark.pdf'])],
     options={'py2exe': {'bundle_files': 2, 'compressed': True}},
     zipfile=None,
     windows=[{'script':'tkinter_app.py',
