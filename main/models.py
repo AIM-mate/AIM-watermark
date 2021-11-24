e8d099658c5c01d1c61576cf7f9da1abf77cc026
jstringara <jstringara@gmail.com>
2021-11-24 01:38:09 +0100

    Prima versione del modello

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Doc(models.Model):
+
+    time= models.DateTimeField(auto_now=True)
+    file = models.FileField(upload_to='documents/')
+
+    def __str__(self):
+        return str(self.pk)
