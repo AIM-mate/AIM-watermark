a73805eb6734f5053323c467905397c957052157
jstringara <jstringara@gmail.com>
2021-11-24 02:13:53 +0100

    Prima versione dell'upload

--- main/views.py
+++ main/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .models import Doc
 
 # Create your views here.
 def index(response):
     return render(response, 'main/index.html')
+
+def upload(request):
+
+    if request.method == 'POST':
+        my_file = request.FILES.get('file')
+        Doc.objects.create(file=my_file)
+        return HttpResponse('')
+
+    return JsonResponse({'post': 'false'})
