07c226ebe1ab49f5f818f73ad18c8666607fca29
jstringara <jstringara@gmail.com>
2021-11-25 01:38:00 +0100

    Aggiunto campo della data

--- main/models.py
+++ main/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 class Doc(models.Model):
 
+    upload_date = models.DateTimeField(default=timezone.now)
     file = models.FileField(upload_to='documents/')
 
     def __str__(self):
         return str(self.pk)
+
